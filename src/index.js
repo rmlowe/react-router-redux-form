e642cfe838d5a2343509e73f2c0a2acc96ede083
Robert Lowe <robertmlowe@rmlowe.com>
2018-05-29 00:11:39 +0100

    Route Design

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ ReactDOM.render(
     <BrowserRouter>
       <div>
          Header
-         <Route path="/hello" component={Hello} />
+         <Route path="/abcd" component={Hello} />
          <Route path="/goodbye" component={Goodbye} />
       </div>
     </BrowserRouter>
